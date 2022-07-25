38fab301312f7fa0d6a0e02f9c4a31640f7b5122
nikitazapekin <70286150+nikitazapekin@users.noreply.github.com>
2022-07-25 22:04:27 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("sidenav1").style.width="250px";
+    document.getElementById("btn").style.opacity="0";
+}
+
+
+function closeNav(){
+    document.getElementById("sidenav1").style.width="0px";
+    document.getElementById("btn").style.opacity="1";
+}
